3cd6084318a70654e2654b4c5ace2d34992a9b5e
dominic.rathbone <domr115@googlemail.com>
2015-12-01 21:12:28 +0000

    partially working web app prototype to test signalling server with

--- webapp/js/app.js
+++
@@ -1,8 +0,0 @@
-
-$(document).ready(function() {
-    $("#connectButton").click(function(){
-        var roomId = $("#roomId").val();
-        console.log(roomId);
-        connect(roomId);
-    });
-});
